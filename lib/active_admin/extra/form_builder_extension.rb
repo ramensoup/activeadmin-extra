674119735b5004355ec78b31c49526b33adf5248
Stefano Pau <stefano.pau@welaika.com>
2014-11-26 11:54:28 +0100

    Update form_builder_extension.rb

--- lib/active_admin/extra/form_builder_extension.rb
+++ lib/active_admin/extra/form_builder_extension.rb
@@ -33,7 +33,7 @@ module ActiveAdmin
 
         content = with_new_form_buffer do
           template.content_tag :div, :class => "has_many #{association} #{options[:class]}" do
-            output_buffer.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
+            template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
 
             options[:class] ||= ""
             options[:class] = "#{options[:class]} inputs has_many_fields".strip
@@ -51,17 +51,16 @@ module ActiveAdmin
             js = template.escape_javascript(js)
             js = template.link_to I18n.t('active_admin.has_many_new', :model => object.class.reflect_on_association(association).klass.model_name.human), "#", :onclick => "$(this).before('#{js}'.replace(/NEW_RECORD/g, new Date().getTime())); return false;", :class => "button"
 
-            output_buffer.last << js.html_safe
+            js.html_safe
           end
         end
-        output_buffer.last << content.html_safe
+        content.html_safe
       end
 
       def destroy(options = {})
         unless object.new_record?
           input :_destroy, { :as => :boolean }.reverse_merge(options)
         end
-        output_buffer.last
       end
 
       def errors
