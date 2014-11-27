5c2eb7d569bcad9630f4efe4e31700126343f423
Stefano Pau <stefano.pau@welaika.com>
2014-11-27 13:01:25 +0100

    Update form_builder_extension.rb

--- lib/active_admin/extra/form_builder_extension.rb
+++ lib/active_admin/extra/form_builder_extension.rb
@@ -31,7 +31,7 @@ module ActiveAdmin
           contents
         end
 
-        content = without_wrapper do
+        content = with_new_form_buffer do
           template.content_tag :div, :class => "has_many #{association} #{options[:class]}" do
             template.output_buffer.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
 
@@ -40,7 +40,7 @@ module ActiveAdmin
             inputs options, &form_block
 
             # Capture the ADD JS
-            js = without_wrapper do
+            js = with_new_form_buffer do
               inputs_for_nested_attributes  :for => [association, object.class.reflect_on_association(association).klass.new],
                                             :class => options[:class],
                                             :for_options => {
