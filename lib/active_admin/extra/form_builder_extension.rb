b6879e7af0d1835244b0d1aaf2ed32a322619a97
Stefano Pau <stefano.pau@welaika.com>
2014-11-27 11:51:23 +0100

    Update form_builder_extension.rb

--- lib/active_admin/extra/form_builder_extension.rb
+++ lib/active_admin/extra/form_builder_extension.rb
@@ -31,16 +31,16 @@ module ActiveAdmin
           contents
         end
 
-        content = template.capture do
+        content = without_wrapper do
           template.content_tag :div, :class => "has_many #{association} #{options[:class]}" do
-            template.output_buffer.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
+            already_in_an_inputs_block.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
     
             options[:class] ||= ""
             options[:class] = "#{options[:class]} inputs has_many_fields".strip
             inputs options, &form_block
 
             # Capture the ADD JS
-            js = template.capture do
+            js = without_wrapper do
               inputs_for_nested_attributes  :for => [association, object.class.reflect_on_association(association).klass.new],
                                             :class => options[:class],
                                             :for_options => {
@@ -51,17 +51,17 @@ module ActiveAdmin
             js = template.escape_javascript(js)
             js = template.link_to I18n.t('active_admin.has_many_new', :model => object.class.reflect_on_association(association).klass.model_name.human), "#", :onclick => "$(this).before('#{js}'.replace(/NEW_RECORD/g, new Date().getTime())); return false;", :class => "button"
 
-            template.output_buffer.last << js.html_safe
+            already_in_an_inputs_block.last << js.html_safe
           end
         end
-        template.output_buffer.last << content.html_safe
+        already_in_an_inputs_block.last << content.html_safe
       end
 
       def destroy(options = {})
         unless object.new_record?
           input :_destroy, { :as => :boolean }.reverse_merge(options)
         end
-        template.output_buffer.last
+        already_in_an_inputs_block.last
       end
 
       def errors
