4f9cb786aeae05376f7a28825aea92bd45593e18
Stefano Pau <stefano.pau@welaika.com>
2014-11-27 10:28:44 +0100

    Update form_builder_extension.rb

--- lib/active_admin/extra/form_builder_extension.rb
+++ lib/active_admin/extra/form_builder_extension.rb
@@ -33,8 +33,8 @@ module ActiveAdmin
 
         content = with_new_form_buffer do
           template.content_tag :div, :class => "has_many #{association} #{options[:class]}" do
-            # form_buffers.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
-            template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
+            @buffer.last << template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
+            #template.content_tag(:h3, options[:label] || object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
 
             options[:class] ||= ""
             options[:class] = "#{options[:class]} inputs has_many_fields".strip
@@ -52,19 +52,19 @@ module ActiveAdmin
             js = template.escape_javascript(js)
             js = template.link_to I18n.t('active_admin.has_many_new', :model => object.class.reflect_on_association(association).klass.model_name.human), "#", :onclick => "$(this).before('#{js}'.replace(/NEW_RECORD/g, new Date().getTime())); return false;", :class => "button"
 
-            #form_buffers.last << js.html_safe
-            js.html_safe
+            @buffers.last << js.html_safe
+            #js.html_safe
           end
         end
-        # form_buffers.last << content.html_safe
-        content.html_safe
+        @buffers.last << content.html_safe
+        #content.html_safe
       end
 
       def destroy(options = {})
         unless object.new_record?
           input :_destroy, { :as => :boolean }.reverse_merge(options)
         end
-        # form_buffers.last
+        @buffers.last
       end
 
       def errors
